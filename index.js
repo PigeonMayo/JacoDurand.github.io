9bbf688a9fb847ff94bb7a06792080421da0d2c4
PigeonMayo <jacodurand02@gmail.com>
2024-12-09 10:48:44 +0200

    added blocks for experience

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 
-// Fetch and insert helloWorldBanner HTML content
+// HEADER
 fetch('banners/banner_name/banner_name.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('banner_name').innerHTML = data;
  
-    // Dynamically load the banner_name.js script
  const script = document.createElement('script');
  script.src = 'banners/banner_name/banner_name.js';
- document.body.appendChild(script); // Or append to <head> if necessary
+ document.body.appendChild(script); 
 
 
 })
@@ -17,11 +16,29 @@ fetch('banners/banner_name/banner_name.html')
     console.error('Error loading banner:', error);
   });
 
-// Fetch and insert helloWorldBanner HTML content
+
+
+// ABOUT
 fetch('banners/banner_about/banner_about.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('banner_about').innerHTML = data;
+
+})
+  .catch(error => {
+    console.error('Error loading banner:', error);
+  });
+
+
+// EXPERIENCE
+fetch('banners/banner_experience/banner_experience.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('banner_experience').innerHTML = data;
+
+    const script = document.createElement('script');
+    script.src = 'banners/banner_experience/banner_experience.js';
+    document.body.appendChild(script); 
 
 })
   .catch(error => {
