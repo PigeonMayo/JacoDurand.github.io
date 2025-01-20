a34737a87c315d7f015e6de13f30881de399e2ec
PigeonMayo <jacodurand02@gmail.com>
2025-01-20 22:14:07 +0200

    added some styling to experience

--- banners/banner_experience/banner_experience.js
+++ banners/banner_experience/banner_experience.js
@@ -1,14 +1,7 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const toggles = document.querySelectorAll('.faq-toggle')
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+toggles.forEach(toggle=> {
+    toggle.addEventListener('click', ()=> {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
